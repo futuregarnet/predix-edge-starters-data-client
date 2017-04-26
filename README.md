736ff3d5e37471b722afbf6b41ce1f351ff13ba2
futuregarnet <futuregarnet@gmail.com>
2017-04-25 21:10:24 -0400

    Added API call to query a day's worth of data.

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ mvn clean install
 ```
 
 Copy the config/application-template.properties to config/application.properties and insert your Predix services information.
+
+Copy the manifest-template.yml to manifest.yml and insert your Predix services information.
+**NOTE**: For the predix_oauth_clientId env varible, you will need to Base64 encode your colon separated client-id:client-password.
+
+## TODO List
+- [x] Create initial working release
+- [ ] Integrate Java-based testing framework
+- [ ] Integrate server side logging conventions
+- [ ] Expand documention on README.md
--- src/main/java/com/ge/predix/solsvc/controller/DataClientController.java
+++ src/main/java/com/ge/predix/solsvc/controller/DataClientController.java
@@ -1,6 +1,9 @@
 package com.ge.predix.solsvc.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import org.apache.http.Header;
@@ -13,8 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ge.predix.entity.timeseries.datapoints.queryrequest.DatapointsQuery;
+import com.ge.predix.entity.timeseries.datapoints.queryrequest.Filters;
 import com.ge.predix.entity.timeseries.datapoints.queryrequest.latest.DatapointsLatestQuery;
 import com.ge.predix.entity.timeseries.datapoints.queryresponse.DatapointsResponse;
+import com.ge.predix.entity.util.map.Map;
 import com.ge.predix.solsvc.timeseries.bootstrap.client.TimeseriesClient;
 
 @RestController
@@ -29,8 +34,10 @@ public class DataClientController {
     @RequestMapping(value = "/latest/{sensorName}/{assetType}/{id}", method = RequestMethod.GET)
     public String getLatestDatapoints(@PathVariable String sensorName, @PathVariable String assetType, @PathVariable String id) throws JsonProcessingException {
         com.ge.predix.entity.timeseries.datapoints.queryrequest.latest.Tag tag = new com.ge.predix.entity.timeseries.datapoints.queryrequest.latest.Tag();
-        tag.setName(String.join(":", assetType, id, sensorName));
         List<com.ge.predix.entity.timeseries.datapoints.queryrequest.latest.Tag> tags = new ArrayList<>();
+        
+        tag.setName(String.join(":", assetType, id, sensorName));
+        
         tags.add(tag);
         
         DatapointsLatestQuery datapointsLatestQuery = new DatapointsLatestQuery();
@@ -45,10 +52,12 @@ public class DataClientController {
     @RequestMapping(value = "/limit_{limit}/{sensorName}/{assetType}/{id}", method = RequestMethod.GET)
     public String getLimitedDatapoints(@PathVariable int limit, @PathVariable String sensorName, @PathVariable String assetType, @PathVariable String id) throws JsonProcessingException {
         com.ge.predix.entity.timeseries.datapoints.queryrequest.Tag tag = new com.ge.predix.entity.timeseries.datapoints.queryrequest.Tag();
+        List<com.ge.predix.entity.timeseries.datapoints.queryrequest.Tag> tags = new ArrayList<>();
+        
         tag.setName(String.join(":", assetType, id, sensorName));
         tag.setLimit(limit);
         tag.setOrder("desc");
-        List<com.ge.predix.entity.timeseries.datapoints.queryrequest.Tag> tags = new ArrayList<>();
+        
         tags.add(tag);
         
         DatapointsQuery datapointsQuery = new DatapointsQuery();
@@ -59,5 +68,42 @@ public class DataClientController {
         DatapointsResponse datapointsResponse = timeseriesClient.queryForDatapoints(datapointsQuery, headers);
         
         return jsonMapper.writeValueAsString(datapointsResponse);
+    }
+    
+    @SuppressWarnings("unchecked")
+    @RequestMapping(value = "/day/{day}/{sensorName}/{assetType}/{id}", method = RequestMethod.GET)
+    public String getTimeBoundRequest(@PathVariable String day, @PathVariable String sensorName, @PathVariable String assetType, @PathVariable String id) throws JsonProcessingException {
+        com.ge.predix.entity.timeseries.datapoints.queryrequest.Tag tag = new com.ge.predix.entity.timeseries.datapoints.queryrequest.Tag();
+        List<com.ge.predix.entity.timeseries.datapoints.queryrequest.Tag> tags = new ArrayList<>();
+
+        Map attribute = new Map();
+        attribute.put("datatype", "FLOAT");
+        Filters attrFilters = new Filters();
+        attrFilters.setAttributes(attribute);
+        tag.setFilters(attrFilters);
+        
+        tags.add(tag);
+        tag.setName(String.join(":", assetType, id, sensorName));
+        tag.setOrder("desc");
+       
+        DatapointsQuery datapointsQuery = new DatapointsQuery();
+        
+        Calendar date = Calendar.getInstance();
+        SimpleDateFormat dayFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            date.setTime(dayFormat.parse(day));
+            long epoch = date.getTimeInMillis();
+            
+            datapointsQuery.setStart(epoch);
+            datapointsQuery.setEnd(epoch + 86399999); // Start at last millisecond of the day
+            datapointsQuery.setTags(tags);
+            
+            List<Header> headers = timeseriesClient.getTimeseriesHeaders();
+            DatapointsResponse datapointsResponse = timeseriesClient.queryForDatapoints(datapointsQuery, headers);
+            
+            return jsonMapper.writeValueAsString(datapointsResponse);
+        } catch (ParseException e) {
+            return jsonMapper.writeValueAsString("ERROR: Unparsable date. Date must be in yyyy-MM-dd format.");
+        }
     }
 }
